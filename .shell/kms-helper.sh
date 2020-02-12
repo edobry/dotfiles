3cd84f15ccc90f54ae49ae7b1e6709ec99bae1e8
Eugene Dobry <edobry@gmail.com>
2020-02-12 12:21:01 -0500

    extract kms key arn

--- .shell/kms-helper.sh
+++ .shell/kms-helper.sh
@@ -1,5 +1,5 @@
 function kmsEncrypt() {
     aws-dev;
-    aws kms encrypt --key-id arn:aws:kms:eu-west-1:477596031399:key/c5e8935f-75e2-4010-bc85-7629d9897374 --plaintext $1 --output text | awk '{ print $1; }';
+    aws kms encrypt --key-id $AWS_KMS_KEY_ARN --plaintext $1 --output text | awk '{ print $1; }';
 }
 
