41c59057070080eb150f8d740fdbd2ef090f4d8b
Eugene Dobry <edobry@gmail.com>
2020-02-12 12:14:22 -0500

    remove old jenkins alias

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -1,11 +1,8 @@
 alias please='sudo $(fc -ln -1)'
 
-alias jenkins='java -jar ~/apps/jenkins-cli/jenkins-cli.jar -s "https://helblindi.chainalysis.com/" -ssh -user eugene'
-
 alias editZshrc='nano ~/.zshrc && zsh'
 
 SHELL_DIR="$HOME/.shell"
 
 alias editZplug='nano $SHELL_DIR/zplug.sh && source $SHELL_DIR/zplug.sh && zplug install && zplug load'
-
 alias editAliases='nano $SHELL_DIR/aliases.sh && source $SHELL_DIR/aliases.sh'
