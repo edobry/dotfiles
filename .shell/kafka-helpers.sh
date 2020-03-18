9bcf10ba7ef7165e218e31e3a73e12650a52579f
Eugene Dobry <edobry@gmail.com>
2020-03-18 17:24:06 -0400

    improved kafka helpers

--- .shell/kafka-helpers.sh
+++ .shell/kafka-helpers.sh
@@ -1,8 +1,10 @@
-alias kafkacat-d='docker run --tty --interactive --rm edenhill/kafkacat:1.5.0 kafkacat -b 172.27.115.6'
+alias kafkacatd='docker run --tty --interactive --rm edenhill/kafkacat:1.5.0 kafkacat -b'
 
-alias listTopics="kafkacat-d -L | grep 'topic \"' | awk '{ print \$2 }' | sed 's/\"//g'"
+function listTopics() {
+    kafkacatd $1 -L | grep "topic \"" | awk '{ print $2 }' | sed 's/\"//g'
+}
 
 function readTopic() {
-    kakfacat-d -C -t $1 -o -$2 -s avro -r http://172.27.115.6:8081 -qe
+    kafkacatd $1 -C -t $2 -o -$3 -qe
 }
 
