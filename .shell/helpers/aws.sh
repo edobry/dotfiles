34e8ebd5b3707da12e4321cf01cad5d2a6237ae4
Eugene Dobry <edobry@gmail.com>
2020-05-08 18:51:59 -0400

    automatically reauthenticate

--- .shell/helpers/aws.sh
+++ .shell/helpers/aws.sh
@@ -3,23 +3,49 @@
 export GOOGLE_USERNAME=eugene@chainalysis.com
 export DEPT_ROLE=engineering-data
 
-export AWS_PROFILE=org-sso
+export AWS_AUTH_PROFILE=org-sso
+export AWS_PROFILE=$AWS_AUTH_PROFILE
 
 export AWS_HELPER_DIR=~/Projects/terraform/util/aws
 source $AWS_HELPER_DIR/org-sso-helper.sh
 
-alias awsId='aws sts get-caller-identity'
+function awsId() {
+    local id
+    if id=$(aws sts get-caller-identity) 2> /dev/null; then
+        echo $id
+        export AWS_CURRENT_ROLE=$id
+    else
+        return 1
+    fi
+}
 
 export DATAENG_DEV=dataeng-dev-admin
 export DATAENG_PROD=data-warehouse-prod-admin
 
 function awsRole() {
-    awsId | jq '.Arn' | awk -F '/' '{ print $2 }'
+    local id
+    if id=$(awsId); then
+        echo $id | jq '.Arn' | awk -F '/' '{ print $2 }'
+    else
+        return 1
+    fi
 }
 
 function aws-auth() {
+    if ! awsId > /dev/null; then
+        echo "Reauthenticating..."
+        AWS_PROFILE=$AWS_AUTH_PROFILE gimme-aws-creds
+    fi
+
     export AWS_PROFILE=$1
-    echo "Assumed role: $(awsRole)"
+
+    local role
+    if [ -z $AWS_CURRENT_ROLE ]; then
+        role=$AWS_CURRENT_ROLE
+    else
+        role=$(awsRole)
+    fi
+    echo "Assumed role: $role"
 }
 
 alias aws-dataeng-dev='aws-auth $DATAENG_DEV'
