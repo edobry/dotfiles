dbc7ef0053c7a33b645f3a3f628e308384c43abb
Eugene Dobry <edobry@gmail.com>
2020-10-20 17:42:37 -0400

    add TF alias

--- .shell/helpers/terraform.sh
+++ .shell/helpers/terraform.sh
@@ -3,6 +3,7 @@ alias tfmt="terraform fmt"
 alias tfp="terraform plan"
 alias tfa="terraform apply"
 alias tfr="terraform refresh"
+alias tfv="terraform validate"
 
 function tfoj() {
     terraform output -json $*
