d20c944cb45e13353c4c2a0c9de3f263a9a518b7
Eugene Dobry <edobry@gmail.com>
2021-04-22 13:08:15 -0400

    add terragrunt aliases

--- .shell/helpers/terraform.sh
+++ .shell/helpers/terraform.sh
@@ -5,6 +5,16 @@ alias tfa="terraform apply"
 alias tfr="terraform refresh"
 alias tfv="terraform validate"
 
+alias tgi="terragrunt init"
+alias tgiu="terragrunt init -upgrade"
+alias tgfmt="terragrunt fmt"
+alias tgp="terragrunt plan"
+alias tga="terragrunt apply"
+alias tgr="terragrunt refresh"
+alias tgpl="terragrunt providers lock"
+alias tgsl="terragrunt state list"
+
+
 function tfoj() {
     terraform output -json $*
 }
