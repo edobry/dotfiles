8a91244dd5ce8b509b212c7fc39459245c43e52c
edobry <edobry@gmail.com>
2023-05-22 11:39:39 -0400

    extract prompt config

---
+++ .shell/helpers/prompt.sh
@@ -0,0 +1,9 @@
+# https://github.com/romkatv/powerlevel10k/issues/563#issuecomment-1473727359
+printf '\n%.0s' {1..$LINES}
+
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block, everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
