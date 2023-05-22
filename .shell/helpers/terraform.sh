c3ac23e26d810184b2a0db9ae29aeaba1514848b
edobry <edobry@gmail.com>
2023-05-22 13:37:13 -0400

    add tf backend/lock fix helpers

--- .shell/helpers/terraform.sh
+++ .shell/helpers/terraform.sh
@@ -7,11 +7,12 @@ alias tfv="terraform validate"
 
 alias tgi="terragrunt init"
 alias tgiu="terragrunt init -upgrade"
+alias tgir="terragrunt init -reconfigure"
 alias tgfmt="terragrunt fmt"
 alias tgp="terragrunt plan"
 alias tga="terragrunt apply"
 alias tgr="terragrunt refresh"
-alias tgpl="terragrunt providers lock"
+alias tgpl="terragrunt providers lock -platform=darwin_amd64 -platform=linux_amd64"
 alias tgsl="terragrunt state list"
 
 
