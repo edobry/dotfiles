62d1c7eb1fbfa36b1cd2d9b6828aba0c1c23e08f
edobry <edobry@gmail.com>
2023-05-22 11:37:34 -0400

    configure history

---
+++ .shell/helpers/history.sh
@@ -0,0 +1,7 @@
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=10000
+SAVEHIST=100000
+setopt appendhistory autocd notify
+bindkey -e
+# End of lines configured by zsh-newuser-installsett
