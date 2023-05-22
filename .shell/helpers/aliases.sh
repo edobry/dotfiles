988b692f221bd3b061df8e0e235d0e49f4e4f3ca
edobry <edobry@gmail.com>
2023-05-22 12:36:09 -0400

    improve meta helpers

--- .shell/helpers/aliases.sh
+++ .shell/helpers/aliases.sh
@@ -1,21 +1,5 @@
 alias please='sudo $(fc -ln -1)'
 
-alias editZshrc='nano ~/.zshrc && zsh'
-
-SHELL_DIR="$HOME/.shell/helpers"
-
-alias editZplug='nano $SHELL_DIR/zplug-env.sh && source $SHELL_DIR/zplug-env.sh && zplug install && zplug load'
-alias editAliases='nano $SHELL_DIR/aliases.sh && source $SHELL_DIR/aliases.sh'
-#
-# alias reloadHelpers='for f in $SHELL_DIR/helpers/*; do source $f; done'
-# alias reloadEnv='for f in $SHELL_DIR/*-env.sh; do source $f; done'
-
-function editHelpers() {
-    local file=$SHELL_DIR/helpers/${1}.sh
-    nano $file
-    source $file
-}
-
 alias kv='kubectl version'
 
 alias resetAudio='sudo pkill -9 coreaudiod'
--- .shell/helpers/meta.sh
+++ .shell/helpers/meta.sh
@@ -1,6 +1,61 @@
+DC_DIR="$HOME/.shell"
+DC_HELPERS_DIR="$DC_DIR/helpers"
+
 alias dotconfig='/usr/bin/git --git-dir=$HOME/.files/ --work-tree=$HOME'
 alias dotc='dotconfig'
-alias dotcs='dotconfig status'
+alias dotcs='dotconfig status | less -R'
+alias dotclog='dotconfig log --graph --decorate --all --color | less -R'
+alias dotcd='cd $DC_DIR'
+
+alias dotcls='ls $DC_HELPERS_DIR'
+alias doterc='$EDITOR $HOME/.zshrc && exec zsh'
+alias dotcd='dotconfig diff'
 alias dotca='dotconfig add'
+alias dotcap='dotconfig add -p'
 alias dotcc='dotconfig commit -m'
 alias dotcp='dotconfig push'
+
+function dotcat() {
+    bat "$DC_DIR/$1"
+}
+
+function dotcath() {
+    bat "$DC_HELPERS_DIR/$1"
+}
+
+function dotce() {
+    $EDITOR "$DC_DIR/$1"
+}
+
+function dotcdh() {
+    dotconfig diff "$DC_HELPERS_DIR/$1"
+}
+
+function dotceh() {
+    $EDITOR "$DC_HELPERS_DIR/$1"
+}
+
+function dotcehr() {
+    dotceh $1 && dotcrh $1
+}
+
+function dotcrh() {
+    source "$DC_HELPERS_DIR/$1"
+}
+
+alias dotcea='dotcehr aliases.sh'
+
+function dotcezi() {
+    dotcehr zinit-env.sh
+    zinit install && zinit load
+}
+
+function dotcah() {
+    dotca "$DC_HELPERS_DIR/$1"
+}
+
+function dotcaph() {
+    dotcap "$DC_HELPERS_DIR/$1"
+}
+
+alias dotcem='dotceh meta.sh'
