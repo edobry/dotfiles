1f18b1a536dd8b194245aaff6c16aaa8c5754588
Eugene Dobry <eugene@dobry.me>
2024-04-10 23:04:36 -0400

    update install script

--- .shell/scripts/macos.sh
+++ .shell/scripts/macos.sh
@@ -1,53 +1,81 @@
+#!/bin/bash
+
+function log() {
+    echo -e "\n$1..."
+}
+
+function waitForUser() {
+    read -r -p "Press Enter to continue"
+    echo
+}
+
 function macosSetup() {
     local email=$1
     local ghToken=$2
 
     # configure macos settings
-    ## disable spotlight indexing
+    echo "disable spotlight indexing..."
     sudo mdutil -i off
+    waitForUser
 
     # prereqs
-    ## XCode Developer Tools
+    log "install XCode Developer Tools"
     xcode-select --install
+    waitForUser
 
-    # setup git
+    setup git
+    log "configure git"
     gitSetup $email $ghToken
 
-    # setup package managers
+    log "setup package managers"
 
     ## setup brew
+    log "install brew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
     brew analytics off
+    waitForUser
 
-    ## setup tea
-    sh <(curl [https://tea.xyz](https://tea.xyz/))
+    # setup tea
+    log "install tea"
+    sh <(curl https://tea.xyz)
+    waitForUser
 
-    # install packages
-    local brewfileName="Brewfile-core"
-    local brewfilePath="/tmp/$brewfileName"
-    curl -fLo $brewfilePath https://raw.githubusercontent.com/edobry/dotfiles/master/resources/$brewfileName
-    brew bundle install --file=$brewfilePath
-
-    # configure shell
+    log "install packages"
     
-    ## install iTerm shell integration
-    curl -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
+    log "fetching Brewfile"
+    local brewfileName="core.Brewfile"
+    local brewfilePath="/tmp/$brewfileName"
+    curl -fLo $brewfilePath https://raw.githubusercontent.com/edobry/dotfiles/master/.shell/resources/$brewfileName
 
-    # setup zsh
+    brew bundle install --file=$brewfilePath
+    waitForUser
 
-    ## setup zinit
-    ### setup subversion?
-    echo
-    bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+    log "configure shell"
+    
+    log "install iTerm shell integration"
+    curl -L https://iterm2.com/shell_integration/zsh | bash
+    waitForUser
 
-    ## install menlo lgs NF font
-    cd ~/Library/Fonts && curl -fLo MesloLGSNerdFontMono-Regular.ttf https://github.com/ryanoasis/nerd-fonts/raw/v3.0.0/patched-fonts/Meslo/S/Regular/MesloLGSNerdFontMono-Regular.ttf
-    mv ~/Library/Fonts/MesloLGSNerdFontMono-Regular.ttf
+    # log "setup zsh"
 
-    ## configure iTerm manually
-    ### - iTerm Settings > Profiles > Default > Text > Font
-    ### - Select "MesloLGS Nerd Font Mono"
-    ### - Enable "Use ligatures"
+    # log "setup zinit"
+    
+    # bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+
+    log "install menlo lgs NF font"
+    local fontsPath=~/Library/Fonts
+    local fontName=MesloLGSNerdFontMono-Regular.ttf
+    curl -fLo $fontsPath/$fontName https://github.com/ryanoasis/nerd-fonts/raw/v3.0.0/patched-fonts/Meslo/S/Regular/$fontName
+
+    # ## configure iTerm manually
+    # ### - iTerm Settings > Profiles > Default > Text > Font
+    # ### - Select "MesloLGS Nerd Font Mono"
+    # ### - Enable "Use ligatures"
+
+    # log "setup dotfiles"
+    # git clone --bare git@github.com:edobry/dotfiles.git $HOME/.files
+    # git --git-dir=$HOME/.files/ --work-tree=$HOME checkout -f
 }
 
 function gitSetup() {
@@ -55,9 +83,12 @@ function gitSetup() {
     local ghToken=$2
 
     local keyPath="~/.ssh/id_github"
+
+    echo "generate ssh key..."
     ssh-keygen -t ed25519 -C "$email" -f $keyPath
     ssh-add --apple-use-keychain $keyPath
-    cat $keyPath.pub | toClip
+
+    echo "add ssh key to Github..."
     githubAddSshKey $ghToken $keyPath "$(uname -n)-cli"
 
     ## configure git to use ssh for all Github
@@ -83,4 +114,5 @@ function githubAddSshKey() {
 }
 
 echo "Starting first-time MacOS setup..."
-macosSetup eugene@dobry.me
+waitForUser
+macosSetup "<email>" "<PAT>"
