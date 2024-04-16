9640baae5e211f94883d5d50dc209457861ed33e
Eugene Dobry <eugene@dobry.me>
2024-04-16 17:13:38 -0400

    fixed zinit OMZP issues due to githubs svn sunsetting

--- .shell/helpers/zinit-env.zsh
+++ .shell/helpers/zinit-env.zsh
@@ -13,35 +13,73 @@ source "${ZINIT_HOME}/zinit.zsh"
 zinit ice lucid atload'source ~/.p10k.zsh; _p9k_precmd; p10k finalize' nocd
 zinit light romkatv/powerlevel10k
 
+# https://github.com/zdharma-continuum/zinit/issues/481#issuecomment-1480192402
+function zinitSnippetOmz() {
+   local snippet=$1
+   local type=${2:-OMZ}
+
+   # init repo if not exists
+   local dir="$ZINIT_INSTALL_DIR/snippets/OMZ::plugins"
+   local omzRepoDir=$dir/ohmyzsh
+   if [[ ! -d "$omzRepoDir" ]]; then
+      (( !OPTS[opt_-q,--quiet] )) && +zi-log "{i} Initializing sparse {file}OMZ::plugins{rst} repository"
+
+      mkdir -p $omzRepoDir && pushd $dir > /dev/null
+      git clone -n --depth=1 --filter=tree:0 https://github.com/ohmyzsh/ohmyzsh
+   elif [[ -d "$dir/$snippet" ]]; then
+      return
+   fi
+
+   # prefix snippet name with "plugins" if type is OMZ
+   local snippetName=$([[ $type == "OMZ" ]] && echo plugins/$snippet || echo $snippet)
+
+   (( !OPTS[opt_-q,--quiet] )) && +zi-log "{nl}{i} Initializing {file}$snippetName{rst} (with git sparse-checkout)"
+   (( !OPTS[opt_-q,--quiet] )) && +zi-log "{info}Note{info}:{msg} See {url}https://github.com/zdharma-continuum/zinit/issues/504#issuecomment-1887858354{msg} for details"
+
+   pushd $omzRepoDir > /dev/null
+   local pluginDir=plugins/$snippet
+   (( !OPTS[opt_-q,--quiet] )) && +zi-log "{i} Shallow cloning & installing{rst}"
+   git sparse-checkout set --no-cone $pluginDir > /dev/null 2>&1
+   git checkout > /dev/null 2>&1
+   cp -R $pluginDir ../$snippet
+   popd > /dev/null
+
+   zinit snippet $type::$snippetName
+}
+
 if isMacOS; then
-    zinit ice svn
-    zinit snippet OMZ::plugins/macos
+   zinitSnippetOmz macos
 fi
 
-zinit ice svn
-zinit snippet OMZ::plugins/aws
+# shell features
 
-zinit ice svn
-zinit snippet OMZP::kubectl
-
-zinit ice svn
 zinit load "agkozak/zsh-z"
 
 zinit light "DarrinTisdale/zsh-aliases-exa"
 
-zinit ice as"completion"
-zinit light "gradle/gradle-completion"
+zinitSnippetOmz history-substring-search
 
-zinit load "Dbz/kube-aliases"
-
-zinit ice svn
-zinit snippet OMZP::history-substring-search
-
-# zsh-fzf-history-search
 zinit ice lucid wait'0'
 zinit light joshskidmore/zsh-fzf-history-search
 
+# shell utils
+
 zinit light "reegnz/jq-zsh-plugin"
+
+# runtimes - java
+
+zinit ice as"completion"
+zinit light "gradle/gradle-completion"
+
+# aws
+
+zinitSnippetOmz aws OMZP
+
+# k8s
+
+zinitSnippetOmz kubectl OMZP
+
+zinit load "Dbz/kube-aliases"
 
 zinit light-mode lucid wait has"kubectl" for \
    id-as"kubectl_completion" \
@@ -83,6 +121,8 @@ zinit light-mode lucid wait has"fnm" for \
    run-atpull \
    zdharma-continuum/null
 
+zinit ice wait"1" lucid
+zinit light "Aloxaf/fzf-tab"
 
 function zinitCompSetup() {
    zstyle ':completion:*' completer _expand _complete _ignored _correct _approximate
@@ -100,9 +140,6 @@ function zinitCompSetup() {
    # set descriptions format to enable group support
    zstyle ':completion:*:descriptions' format '[%d]'
 }
-
-zinit ice wait"1" lucid
-zinit light "Aloxaf/fzf-tab"
 
 zinit wait"0a" lucid for \
 atclone"zinit creinstall -q /usr/share/zsh/site-functions" \
