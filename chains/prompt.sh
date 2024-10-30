166dc478b3c62f9de6895267ca7b2d311aa17402
Eugene Dobry <edobry@gmail.com>
2024-10-30 11:58:12 -0400

    pull out p10k envvar

--- chains/prompt.sh
+++ chains/prompt.sh
@@ -4,6 +4,7 @@ printf '\n%.0s' {1..$LINES}
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block, everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+P10K_PATH="${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+if [[ -r "$P10K_PATH" ]]; then
+  source "$P10K_PATH"
 fi
