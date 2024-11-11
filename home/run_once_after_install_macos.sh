17c4d73e27718a41fcedcb5aa42a6a991b570a96
Eugene Dobry <edobry@gmail.com>
2024-11-11 17:01:48 -0500

    bring iterm-cli & menlo fonts into chitin install

--- home/run_once_after_install_macos.sh
+++ home/run_once_after_install_macos.sh
@@ -19,43 +19,52 @@ function macosSetup() {
     waitForUser
 
     # prereqs
-    log "install XCode Developer Tools"
-    xcode-select --install
-    waitForUser
+    # log "install XCode Developer Tools"
+    # xcode-select --install
+    # waitForUser
 
-    setup git
+    # setup git
     log "configure git"
     gitSetup $email $ghToken
 
     log "setup package managers"
 
-    ## setup brew
-    log "install brew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-    brew analytics off
-    waitForUser
+    # setup brew
+    # log "install brew"
+    # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # eval "$(/opt/homebrew/bin/brew shellenv)"
+    # brew analytics off
+    # waitForUser
 
     # setup tea
-    log "install tea"
-    sh <(curl https://tea.xyz)
-    waitForUser
+    # log "install tea"
+    # sh <(curl https://tea.xyz)
+    # waitForUser
+
+    local chitinTempDir="/tmp/chitin-install"
+
+    # log "install temporary jq"
+    # local jqVersion="1.6"
+    # local jqTempPath="$chitinTempDir/jq"
+    # curl -sL https://github.com/stedolan/jq/releases/download/jq-$jqVersion/jq-osx-amd64 -o "$jqTempPath"
+    # chmod +x "$jqTempPath"
+
 
     log "install packages"
     
-    log "fetching Brewfile"
-    local brewfileName="core.Brewfile"
-    local brewfilePath="/tmp/$brewfileName"
-    curl -fLo $brewfilePath https://raw.githubusercontent.com/edobry/dotfiles/master/.shell/resources/$brewfileName
+    # log "fetching Brewfile"
+    # local brewfileName="core.Brewfile"
+    # local brewfilePath="/tmp/$brewfileName"
+    # curl -fLo $brewfilePath https://raw.githubusercontent.com/edobry/dotfiles/master/.shell/resources/$brewfileName
 
-    brew bundle install --file=$brewfilePath
-    waitForUser
+    # brew bundle install --file=$brewfilePath
+    # waitForUser
 
     log "configure shell"
     
-    log "install iTerm shell integration"
-    curl -L https://iterm2.com/shell_integration/zsh | bash
-    waitForUser
+    # log "install iTerm shell integration"
+    # curl -L https://iterm2.com/shell_integration/zsh | bash
+    # waitForUser
 
     # log "setup zsh"
 
@@ -63,10 +72,10 @@ function macosSetup() {
     
     # bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
-    log "install menlo lgs NF font"
-    local fontsPath=~/Library/Fonts
-    local fontName=MesloLGSNerdFontMono-Regular.ttf
-    curl -fLo $fontsPath/$fontName https://github.com/ryanoasis/nerd-fonts/raw/v3.0.0/patched-fonts/Meslo/S/Regular/$fontName
+    # log "install menlo lgs NF font"
+    # local fontsPath=~/Library/Fonts
+    # local fontName=MesloLGSNerdFontMono-Regular.ttf
+    # curl -fLo $fontsPath/$fontName https://github.com/ryanoasis/nerd-fonts/raw/v3.0.0/patched-fonts/Meslo/S/Regular/$fontName
 
     # ## configure iTerm manually
     # ### - iTerm Settings > Profiles > Default > Text > Font
