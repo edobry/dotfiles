f07cab4c57c2bb4de8f2672ec7c12a6c927deddb
Eugene Dobry <eugene@dobry.me>
2024-11-14 21:04:35 -0500

    move auth chain and add dotAuth helpers

---
+++ chains/auth/auth.sh
@@ -0,0 +1,25 @@
+function bwUnlock() {
+    local sessionKey=$(bw unlock --raw)
+    export BW_SESSION=$sessionKey
+}
+
+function bwGetItems() {
+    [[ -z $BW_SESSION ]] && bwUnlock
+
+    bw list items | jq -r '.[].name'
+}
+
+DOT_AUTH_CREDS_DIR_FIELD="credsDir"
+DOT_AUTH_GPG_KEY_NAME_FIELD="keyName"
+
+function dotAuthGetCredsDir() {
+    expandPath "$(chiConfigChainReadField dotfiles:auth "$DOT_AUTH_CREDS_DIR_FIELD")"
+}
+
+function dotAuthGetKeyName() {
+    chiConfigChainReadField dotfiles:auth "$DOT_AUTH_GPG_KEY_NAME_FIELD"
+}
+
+function gpgImport() {
+    gpg --import "$(dotAuthGetCredsDir)/$(dotAuthGetKeyName)"
+}
